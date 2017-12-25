bee960e782ef6e26af3f28db0a7c4a2e68a8c404
YeaSoft Administrator <info@yeasoft.com>
2017-12-25 17:27:33 +0100

    More info on update

--- backend/updatefl.sh
+++ backend/updatefl.sh
@@ -3,6 +3,8 @@ SCRIPT=$(readlink -f $0)
 SCRIPTDIR=$(dirname ${SCRIPT})
 WORKDIR=${SCRIPTDIR}/tmp
 
+STARTTIME=$(date +%s)
+
 # create working area
 mkdir -p ${WORKDIR}
 
@@ -10,11 +12,16 @@ mkdir -p ${WORKDIR}
 rm -f ${WORKDIR}/Filmliste-akt.xz ${WORKDIR}/Filmliste-akt
 
 # download and upack
+echo "Getting Movie List..."
 wget -P ${WORKDIR} -q http://verteiler2.mediathekview.de/Filmliste-akt.xz
 xz -d ${WORKDIR}/Filmliste-akt.xz
+
+echo "Download took $(( $(date +%s) - ${STARTTIME} )) seconds."
 
 # process
 python ${SCRIPTDIR}/updatefl.py ${WORKDIR}/Filmliste-akt
 
 # clean up
 rm -f ${WORKDIR}/Filmliste-akt.xz ${WORKDIR}/Filmliste-akt
+
+echo "Finished after $(( $(date +%s) - ${STARTTIME} )) seconds."
