36d6b7aae97664905568e0b89f8e0ed005986cdf
CodingPF <j-doo@gmx.de>
2020-05-05 14:29:26 +0200

    move decode into kodiAddon

--- resources/lib/kodi/kodiaddon.py
+++ resources/lib/kodi/kodiaddon.py
@@ -35,8 +35,8 @@ class KodiAddon(KodiLogger):
         self.icon = self.addon.getAddonInfo('icon')
         self.fanart = self.addon.getAddonInfo('fanart')
         self.version = self.addon.getAddonInfo('version')
-        self.path = self.addon.getAddonInfo('path')
-        self.datapath = xbmc.translatePath(self.addon.getAddonInfo('profile')).decode('utf-8')
+        self.path = self.addon.getAddonInfo('path') ##TODO self.unicodePath = unicode(self.path, 'utf-8')
+        self.datapath = xbmc.translatePath(self.addon.getAddonInfo('profile')) ### TODO.decode('utf-8')
         self.language = self.addon.getLocalizedString
         KodiLogger.__init__(self, self.addon_id, self.version)
 
