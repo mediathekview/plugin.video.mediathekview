74d3e7aa62e6fe60bc7208218eb2bbf5cf6422e7
CodingPF <j-doo@gmx.de>
2020-05-05 16:34:18 +0200

    P2 Helper functions

--- resources/lib/mvutils.py
+++ resources/lib/mvutils.py
@@ -33,6 +33,29 @@ except ImportError:
     IS_KODI = False
 
 
+PY2 = sys.version_info[0] == 2
+
+def py2_encode(s, encoding='utf-8'):
+   """
+   Encode Python 2 ``unicode`` to ``str``
+
+   In Python 3 the string is not changed.   
+   """
+   if PY2 and isinstance(s, unicode):
+       s = s.encode(encoding)
+   return s
+
+
+def py2_decode(s, encoding='utf-8'):
+   """
+   Decode Python 2 ``str`` to ``unicode``
+
+   In Python 3 the string is not changed.
+   """
+   if PY2 and isinstance(s, str):
+       s = s.decode(encoding)
+   return s
+
 def dir_exists(name):
     """
     Tests if a directory exists
